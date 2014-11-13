828b6020b239c4eeb110902cf90eff53be948c4f
Gorka <gorka.jusue@d2d.es>
2014-11-13 13:44:55 +0100

    Delete lib folder in app -- bower is necessary

--- web.js
+++ web.js
@@ -5,5 +5,5 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/app'))
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("AngularJS & express server & NodeJS app is running at localhost:" + app.get('port'))
 })
